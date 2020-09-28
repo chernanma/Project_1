1eba61356302e4e5bc2d077b1926ae1d7f47c882
Amit Karmacharya <amitkarmacharya@Amits-MacBook-Pro.local>
2020-09-28 19:43:45 -0400

    Added APIKEY and endpoint for covid location

---
+++ Assets/js/apikeys.js
@@ -0,0 +1,27 @@
+/** 
+ * 
+ *              COVID-19 LOCATION APIKEY AND ENDPOINT
+ * 
+ *                Find the list of all location
+ * 
+ *  website - https://discover.search.hereapi.com/v1/discover
+ * 
+ */ 
+
+// APIKEY 
+const COVID_LOCATION_APIKEY = "lZLEGOEVL9DV9PZrgak4xPaYgxI8C3gtBlB6hWpY0Js";
+
+// API EndPoint
+// example - https://discover.search.hereapi.com/v1/discover?q=Covid&at=30.22,-92.02&limit=10&apikey={{APIKEY}}
+const COVID_LOCATION_ENDPOINT = "https://discover.search.hereapi.com/v1/discover";
+
+
+/** 
+ * 
+ *              COVID-19 INFORMATION AND STATS
+ *  
+ *         Informtion about avtive cases, eaths for different regions
+ * 
+ *  website - https://discover.search.hereapi.com/v1/discover
+ * 
+ */ 
