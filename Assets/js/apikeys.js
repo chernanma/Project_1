8b5fd53dde2df36adfbff7faec775f7e8937e2f4
Cesar Martinez <chernanma@msn.com>
2020-09-30 19:28:50 -0400

    Updating End point Url for Location API

--- Assets/js/apikeys.js
+++ Assets/js/apikeys.js
@@ -13,7 +13,7 @@ export const COVID_LOCATION_APIKEY = "lZLEGOEVL9DV9PZrgak4xPaYgxI8C3gtBlB6hWpY0J
 
 // API EndPoint
 // example - https://discover.search.hereapi.com/v1/discover?q=Covid&at=30.22,-92.02&limit=10&apikey={{APIKEY}}
-export const COVID_LOCATION_ENDPOINT = "https://discover.search.hereapi.com/v1/discover";
+export const COVID_LOCATION_ENDPOINT = "https://discover.search.hereapi.com/v1/discover?q=";
 
 /** 
  * 
