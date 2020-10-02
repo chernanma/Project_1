ecff6a500483658e742139be5c2c270955c22678
Cesar Martinez <chernanma@msn.com>
2020-10-02 08:33:27 -0400

    Adding code to Generate List of Locations Automatically

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -17,22 +17,50 @@ function callLocationAPI(){
     var apiKeyLocation = COVID_LOCATION_APIKEY;
     var Lon = '30.22';
     var Lat = '-92.02';
-    var queryWords ='Covid+Rockville';
+    var queryWords ='Covid+Washington';
     var queryUrlLocation = COVID_LOCATION_ENDPOINT+'?q='+queryWords+'&at='+Lon+','+Lat+'&limit=10&apikey='+apiKeyLocation;
-
+    // making request to Locations API
     $.ajax({
         url: queryUrlLocation,
         method: "GET"        
     }).then(function(response){
-    console.log(response);
-    
-    for (var i = 0;i<response.length;i++){
-        console.log(response.items[i]);
-    }
+    console.log(response.items.length);
+    //Generating List of locations automatically DOM manipulation
+    for (var i = 0;i<response.items.length;i++){
+        console.log(response.items[i]);     
+        
+         var liLocations = $('<li>');
+        var divHeaderLoc = $('<div>');
+        var divBodyLoc=$('<div>');
+        var iHeaderLoc =$('<i>');
+        var spanLoc=$('<span>');
+        divHeaderLoc.attr('class','collapsible-header active');
+        iHeaderLoc.attr('class','material-icons');
+        divBodyLoc.attr('class','collapsible-body blue-grey lighten-4');             
+        spanLoc.text(response.items[i].address.label);        
+        divBodyLoc.append(spanLoc);
+        for(var j=0;j<response.items[i].contacts.length;j++){
+            var ploc=$('<p>').text('Phone :'+ response.items[i].contacts[j].phone[0].value);
+            console.log(ploc.text());
+            divBodyLoc.append(ploc);
+        }               
+        divHeaderLoc.text(response.items[i].title);
+        iHeaderLoc.text('place');        
+        divHeaderLoc.append(iHeaderLoc);
+        liLocations.append(divHeaderLoc);
+        liLocations.append(divBodyLoc);        
+        $('#LiLocations').append(liLocations); 
 
+    }
+   
+    $(document).ready(function(){
+        $('.collapsible').collapsible();
+      });
 
     });
 
 }
-
+// $(document).ready(function(){
+//     $('.collapsible').collapsible();
+// });
 callLocationAPI();
